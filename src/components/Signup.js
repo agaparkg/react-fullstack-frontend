6ef1ca69f9152d20e0ede814472343de6fda7d6b
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-02-14 21:30:48 -0500

    modified Signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
 
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: null,
+      lname: null,
+      email: null,
+      password: null,
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
+    console.log(this.state);
     return (
       <form>
         <h3>Sign Up</h3>
@@ -9,32 +25,44 @@ class Signup extends Component {
         <div className="form-group">
           <label>First name</label>
           <input
+            onChange={this.handleInputChange}
             type="text"
             className="form-control"
             placeholder="First name"
+            name="fname"
           />
         </div>
 
         <div className="form-group">
           <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
+          <input
+            onChange={this.handleInputChange}
+            type="text"
+            className="form-control"
+            placeholder="Last name"
+            name="lname"
+          />
         </div>
 
         <div className="form-group">
           <label>Email address</label>
           <input
+            onChange={this.handleInputChange}
             type="email"
             className="form-control"
             placeholder="Enter email"
+            name="email"
           />
         </div>
 
         <div className="form-group">
           <label>Password</label>
           <input
+            onChange={this.handleInputChange}
             type="password"
             className="form-control"
             placeholder="Enter password"
+            name="password"
           />
         </div>
 
