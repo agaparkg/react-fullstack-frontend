1050ace73fa60d676afb7ccb58afb2de0da8922f
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-02-14 20:20:15 -0500

    added Login and Signup components, setup react router dom configs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-expand-sm navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={"/login"}>
+                React Fullstack by AG
+              </Link>
+              <div
+                className="collapse navbar-collapse"
+                id="navbarTogglerDemo02"
+              >
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/login"}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/register"}>
+                      Sign up
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Signup} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
