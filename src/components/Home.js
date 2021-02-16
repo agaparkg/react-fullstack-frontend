a06113087c3ac44f32d593741232f36a8ea03723
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-02-16 01:39:49 -0500

    added new navbar component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="nav-wrapper">
+      <div className="nav-inner">Home</div>
+    </div>
+  );
+}
+
+export default Home;
