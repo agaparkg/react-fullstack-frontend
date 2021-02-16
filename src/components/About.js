989718693e6dfbe6402dd88c36ea0158ba5c907d
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-02-16 01:39:02 -0500

    added new navbar component

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="nav-wrapper">
+      <div className="nav-inner">About</div>
+    </div>
+  );
+}
+
+export default About;
