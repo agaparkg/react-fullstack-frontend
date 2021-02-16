def89545c8b9979c2420ed2c63fb3fd750df2ba6
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-02-16 01:42:04 -0500

    fetched data from API endpoint and displayed in the UI

--- src/components/People.js
+++ src/components/People.js
@@ -1,43 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Table, Button } from "reactstrap";
 import Person from "./Person";
+import Loading from "./Loading";
 
-const People = ({ people, handleActions }) => {
+const People = () => {
+  const [people, setPeople] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(false);
+
+    const url = "http://localhost:5000/api/v1/people";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          setPeople(data.people);
+          setIsLoading(true);
+        }, 500);
+      })
+      .catch((err) => console.log({ error: err.toString() }));
+  }, []);
+
+  const handleActions = (btn, id = 0) => {
+    if (btn === "edit" || btn === "add") {
+      setModal((isModalOpen) => !isModalOpen);
+    }
+
+    if (btn === "delete") {
+      console.log(id);
+      // fetch(`http://localhost:5000/api/v1/person/delete?id=${id}`, {
+      //   method: "DELETE",
+      // })
+      //   .then((res) => res.json())
+      //   .then((data) => console.log({ message: data.message }))
+      //   .catch((err) => console.log({ error: err.toString() }));
+
+      setPeople((people) => people.filter((person) => person._id !== id));
+    }
+  };
   return (
-    <>
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Age</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {people.map((person, idx) => {
-            return (
-              <Person
-                key={person._id}
-                person={person}
-                idx={idx}
-                handleActions={handleActions}
-              />
-            );
-          })}
-        </tbody>
-      </Table>
-      <Button
-        id="add"
-        onClick={(e) => handleActions(e.target.id)}
-        color="primary"
-      >
-        Add New Person
-      </Button>
-    </>
+    <div className="nav-wrapper">
+      <div className="nav-inner">
+        {!isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <Table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Age</th>
+                  <th>Email</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {people.map((person, idx) => {
+                  return (
+                    <Person
+                      key={person._id}
+                      person={person}
+                      idx={idx}
+                      handleActions={handleActions}
+                    />
+                  );
+                })}
+              </tbody>
+            </Table>
+            <Button
+              id="add"
+              onClick={(e) => handleActions(e.target.id)}
+              color="primary"
+            >
+              Add New Person
+            </Button>
+          </>
+        )}
+      </div>
+    </div>
   );
 };
 
 export default People;
+
+// http://localhost:5000/api/v1/people
