cc4263d831cf061189b21b4b436d5a6118b325cb
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-02-16 01:47:35 -0500

    modified Signup component to reset itself afer successfully adding a person

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -52,80 +52,86 @@ class Signup extends Component {
     const { error, success } = this.state;
 
     return (
-      <form onSubmit={this.handleFormSubmit}>
-        {success && <div className="greeting">{success}</div>}
-        <h3>Sign Up</h3>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <form onSubmit={this.handleFormSubmit}>
+            {success && <div className="greeting">{success}</div>}
+            <h3>Sign Up</h3>
 
-        <div className="form-group">
-          <label>First name</label>
-          <input
-            ref={(input) => (this.fname = input)}
-            type="text"
-            className="form-control"
-            placeholder="First name"
-            name="fname"
-          />
-        </div>
-        {error.includes("fname") && <div style={errorStyle}>{error}</div>}
-        <div className="form-group">
-          <label>Last name</label>
-          <input
-            ref={(input) => (this.lname = input)}
-            type="text"
-            className="form-control"
-            placeholder="Last name"
-            name="lname"
-          />
-        </div>
-        {error.includes("lname") && <div style={errorStyle}>{error}</div>}
+            <div className="form-group">
+              <label>First name</label>
+              <input
+                ref={(input) => (this.fname = input)}
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                name="fname"
+              />
+            </div>
+            {error.includes("fname") && <div style={errorStyle}>{error}</div>}
+            <div className="form-group">
+              <label>Last name</label>
+              <input
+                ref={(input) => (this.lname = input)}
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+                name="lname"
+              />
+            </div>
+            {error.includes("lname") && <div style={errorStyle}>{error}</div>}
 
-        <div className="form-group">
-          <label>Age</label>
-          <input
-            ref={(input) => (this.age = input)}
-            type="number"
-            className="form-control"
-            placeholder="Age"
-            name="age"
-          />
-        </div>
-        {error.includes("age") && <div style={errorStyle}>{error}</div>}
+            <div className="form-group">
+              <label>Age</label>
+              <input
+                ref={(input) => (this.age = input)}
+                type="number"
+                className="form-control"
+                placeholder="Age"
+                name="age"
+              />
+            </div>
+            {error.includes("age") && <div style={errorStyle}>{error}</div>}
 
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            ref={(input) => (this.email = input)}
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            name="email"
-          />
-        </div>
-        {error.includes("email") && <div style={errorStyle}>{error}</div>}
+            <div className="form-group">
+              <label>Email address</label>
+              <input
+                ref={(input) => (this.email = input)}
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                name="email"
+              />
+            </div>
+            {error.includes("email") && <div style={errorStyle}>{error}</div>}
 
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            ref={(input) => (this.password = input)}
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            name="password"
-          />
-        </div>
-        {error.includes("password") && <div style={errorStyle}>{error}</div>}
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                ref={(input) => (this.password = input)}
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                name="password"
+              />
+            </div>
+            {error.includes("password") && (
+              <div style={errorStyle}>{error}</div>
+            )}
 
-        <button
-          disabled={success}
-          type="submit"
-          className="btn btn-primary btn-block"
-        >
-          Sign Up
-        </button>
-        <p className="forgot-password text-right">
-          Already registered <a href="#">sign in?</a>
-        </p>
-      </form>
+            <button
+              disabled={success}
+              type="submit"
+              className="btn btn-primary btn-block"
+            >
+              Sign Up
+            </button>
+            <p className="forgot-password text-right">
+              Already registered <a href="#">sign in?</a>
+            </p>
+          </form>
+        </div>
+      </div>
     );
   }
 }
