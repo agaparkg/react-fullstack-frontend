ae1d7e2f22de40c815b712e8450d376d91442370
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-02-16 01:39:13 -0500

    added new navbar component

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="nav-wrapper">
+      <div className="nav-inner">Contact</div>
+    </div>
+  );
+}
+
+export default Contact;
